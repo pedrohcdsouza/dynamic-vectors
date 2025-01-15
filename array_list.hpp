c6abf5336520be6ee86b0e5c0344e8bfa23a6bd6
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-01-14 22:18:07 -0300

    Adjusting the contents of the file (it had main contents)

--- array_list.hpp
+++ array_list.hpp
@@ -1,39 +1,170 @@
-#include <iostream>
-#include <chrono>
-#include "array_list.hpp"
+#ifndef __ARRAY_LIST_IFRN__
+#define __ARRAY_LIST_IFRN__
 
-int main() {
-    unsigned int n;
-    std::cin >> n;
-    array_list l1;
-    for (unsigned int i = 0; i < n; ++i) {
-        int x;
-        std::cin >> x;
-        l1.push_back(x);
+class array_list {
+private:
+int* data;
+unsigned int size_, capacity_;
+
+void increase_capacity() {
+    capacity_ *= 2;
+    int *new_node = new int[capacity_];
+
+    for (unsigned int i = 0; i < size_; ++i) {
+        new_node[i] = data[i];
     }
-    auto beg = std::chrono::high_resolution_clock::now();
-    unsigned int m;
-    std::cin >> m;
-    unsigned int removed = 0, not_removed = 0;
-    for (int i = 0; i < m; ++i) {
-        int x;
-        std::cin >> x;
-        if (l1.remove_at(x))
-            removed++;
-        else
-            not_removed++;
-    }
-    auto end = std::chrono::high_resolution_clock::now();
-    auto elapsed = end - beg;
-    std::cerr << "[DEBUG] Tried to remove " << m << " element(s)\n";
-    std::cerr << "[DEBUG] removed " << removed << " element(s)\n";
-    std::cerr << "[DEBUG] not removed " << not_removed << " element(s)\n";
-    if (not (l1.size() == n - removed) or (not_removed + removed != m)) {
-        std::cerr << "[ERROR] check remove_at method!\n";
-        exit(1);
-    }
-    std::cerr << "[INFO] " << "Elapsed time for "
-        << removed << " remove_at success : "
-        << elapsed.count() << std::endl;
-    return 0;
+
+    delete[] data;
+    data = new_node;
 }
+
+public:
+array_list() {
+    this-> size_ = 0;
+    this-> capacity_ = 8;
+    this-> data = new int[capacity_];
+}
+~array_list() {
+    delete[] data;
+}
+unsigned int size() {
+    return size_;
+} 
+unsigned int capacity() {
+    return capacity_;
+}
+double percent_occupied() {
+   return (static_cast<double>(size_) / static_cast<double>(capacity_)) * 100;
+} 
+
+bool insert_at(unsigned int index, int value) {
+   if(index > size_) {
+       return false;
+   }
+
+    if(size_ == capacity_) {
+        increase_capacity();
+    }
+
+       for(std::size_t i = size_; i > index; --i) {
+        data[i] = data[i - 1];
+    }
+
+    data[index] = value;
+    ++size_;
+    return true;
+} 
+bool remove_at(unsigned int index) {
+      if(index > size_) {
+       return false;
+   }
+
+       for(std::size_t i = index; i < size_ - 1; ++i) {
+        data[i] = data[i + 1];
+    }
+    
+     --size_;
+     return true;
+} 
+
+int get_at(unsigned int index) {
+    if (index >= size_) { 
+        return -1;
+    }
+    return data[index];
+}
+
+void clear() {
+    delete[] data; 
+    size_ = 0;
+    capacity_ = 8;
+    data = new int[capacity_];
+}
+
+void push_back(int value) {
+    if(size_ == capacity_) {
+        increase_capacity();
+    }
+
+    data[size_] = value;
+    ++size_;
+} 
+void push_front(int value) {
+    if(size_ == capacity_) {
+        increase_capacity();
+    }
+
+
+    for(int i = size_; i > 0; --i) {
+        data[i] = data[i - 1];
+    }
+
+    data[0] = value;
+    ++size_;
+}
+bool pop_back() {
+    if(size_ == 0) {
+        return false;
+    }
+
+    --size_;
+    return true;
+}
+bool pop_front() {
+    if(size_ == 0) {
+        return false;
+    }
+
+    for(std::size_t i = 0; i < size_ - 1; ++i) {
+        data[i] = data[i + 1];
+    }
+
+    --size_;
+    return true;
+}
+int back(){
+    return data[size_ - 1];
+}
+int front(){
+       return data[0];
+} 
+bool remove(int value) {
+    for(std::size_t i = 0; i < size_; ++i) {
+        if(data[i] == value) {
+            for(std::size_t j = i; j < size_ - 1; ++j) {
+                data[j] = data[j + 1];
+            }
+
+            --size_;
+            return true;
+        }
+    }
+    return false;
+}
+int find(int value) {
+    for (std::size_t i = 0; i < size_; ++i) {
+        if (data[i] == value) {
+            return i;
+        }
+    }
+    return -1;
+}
+int count(int value) {
+    int count = 0;
+    for (std::size_t i = 0; i < size_; ++i) {
+        if (data[i] == value) {
+            ++count;
+        }
+    }
+    return count;
+} 
+int sum() {
+    int sum = 0;
+    for (std::size_t i = 0; i < size_; ++i) {
+        sum += data[i];
+    }
+    return sum;
+
+} 
+};
+#endif // __ARRAY_LIST_IFRN__
